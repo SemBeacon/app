557aad9086938c8370be0ba1f5dd9ec738c6051f
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2023-04-26 15:28:09 +0200

    fix: generate resources

--- src/services/SemBeaconService.ts
+++ src/services/SemBeaconService.ts
@@ -1,16 +1,19 @@
 import { BLESemBeacon } from "@/models/BLESemBeacon";
 import { Service } from "@openhps/core";
-import axios from 'axios';
+import { RDFSerializer } from "@openhps/rdf";
+import axios, { AxiosResponse } from 'axios';
 
 export class SemBeaconService extends Service {
-
+    
     protected fetchData(beacon: BLESemBeacon): Promise<any> {
         return new Promise((resolve, reject) => {
             axios.get(beacon.resourceUri, {
                 headers: {
                     Accept: "text/turtle"
                 }
-            }).then(() => {
+            }).then((result: AxiosResponse) => {
+                const deserialized = RDFSerializer.deserializeFromString(beacon.resourceUri, result.data);
+                console.log(deserialized);
                 resolve(undefined);
             }).catch(reject);
         });
