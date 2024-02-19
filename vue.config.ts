5a1b32914cb30c24e2babb0e07c935341c6b6b2e
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2024-02-19 14:29:08 +0100

    fix: webpack issues

--- vue.config.ts
+++ vue.config.ts
@@ -1,17 +1,55 @@
 import { defineConfig } from '@vue/cli-service';
 import CopyPlugin from "copy-webpack-plugin";
 import MonacoWebpackPlugin from "monaco-editor-webpack-plugin";
+import TerserPlugin from "terser-webpack-plugin";
 
 export default defineConfig({
-  terser: {
-    minify: 'uglifyJs',
-    terserOptions: {
-        compress: {
-            drop_console: true,
-        },
-    },
-  },
   configureWebpack: {
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        exclude: [/public/gi, /js\/vendor/gi],
+        parallel: true,
+        terserOptions: {
+          compress: {
+            // turn off flags with small gains to speed up minification
+            arrows: false,
+            collapse_vars: false, // 0.3kb
+            comparisons: false,
+            computed_props: false,
+            hoist_funs: false,
+            hoist_props: false,
+            hoist_vars: false,
+            inline: false,
+            loops: false,
+            negate_iife: false,
+            properties: false,
+            reduce_funcs: false,
+            reduce_vars: false,
+            switches: false,
+            toplevel: false,
+            typeofs: false,
+    
+            // a few flags with noticeable gains/speed ratio
+            // numbers based on out of the box vendor bundle
+            booleans: true, // 0.7kb
+            if_return: true, // 0.4kb
+            sequences: true, // 0.7kb
+            unused: true, // 2.3kb
+    
+            // required features to drop conditional branches
+            conditionals: true,
+            dead_code: true,
+            evaluate: true,
+
+            drop_console: true
+          },
+          mangle: {
+            safari10: true
+          }
+        }
+      })],
+    },
     externals: {
       'path': [],
       '@openhps/core': ['OpenHPS', 'core'],
@@ -20,9 +58,6 @@ export default defineConfig({
       '@openhps/rdf': ['OpenHPS', 'rdf'],
       '@sembeacon/openhps': ['SemBeacon', 'openhps'],
       '@openhps/solid/browser': ['OpenHPS', 'solid'],
-      'leaflet': ['L'],
-      'leaflet-distorableimage': ['L'],
-      'leaflet-toolbar': ['L'],
     },
     plugins: [
       new MonacoWebpackPlugin({
@@ -32,12 +67,9 @@ export default defineConfig({
       new CopyPlugin({
         patterns: [
           { 
-            from: "public", 
-            globOptions: {
-              ignore: ["**/index.html"],
-            } 
+            from: "src/**/*.worker.js", 
+            to: "js/workers/[name].js",
           },
-          { from: "src/**/*.worker.js", to: "js/workers/[name].js" },
         ],
       })
     ]
